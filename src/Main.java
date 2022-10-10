0538b4591f6e0c3f3f34924b12d2f343576d3fb8
Fabricio Valadares <fabricio.valadares@gmail.com>
2022-10-10 20:58:08 -0300

    Primeiro Commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    private static double pi() {
+        return 3.14159265359;
+    }
+
+    private static void impressora(String impressao) {
+        System.out.println(impressao);
+    }
+
+    private static int calcularDobro(int n) {
+        int dobro = n * 2;
+        return dobro;
+    }
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int numero;
+        int retorno;
+        System.out.println("Por favor, informe um número" +
+                "para saber o dobro dele.");
+        numero = entrada.nextInt();
+        retorno = calcularDobro(numero);
+        System.out.println("O dobro de " +
+                numero + " é " +
+                retorno);
+
+        System.out.println(pi());
+        System.out.println("A área do círculo de raio 5 é: "
+                + pi() * Math.pow(5, 2));
+        impressora("USANDO PROCEDIMENTO:: " +
+                "A área do círculo de raio 5 é: "
+                + pi() * Math.pow(5, 2));
+
+
+//  Math -> biblioteca de funções matemáticas.
+//        pow -> potenciação
+//        srqt -> raiz quadrada
+    }
+}
